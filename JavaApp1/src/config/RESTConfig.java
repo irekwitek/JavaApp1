079b81f19b05f33eefcf948f7c385003202a0fd3
irekwitek <irekwitek@hotmail.com>
2017-10-03 14:08:06 -0500

    cm

--- JavaApp1/src/config/RESTConfig.java
+++ JavaApp1/src/config/RESTConfig.java
@@ -6,7 +6,7 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @EnableWebMvc
-@ComponentScan(basePackages = {"com.iwg.controllers"})
+@ComponentScan(basePackages = {"com.iwg"})
 public class RESTConfig {
 
 }
